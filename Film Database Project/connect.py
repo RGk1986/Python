e5cf6de63284a0283350b56a0b63971f8f8a1a5e
RGk1986 <135697724+RGk1986@users.noreply.github.com>
2023-08-15 07:32:28 +0100

    many changes as version control has been terrible.

--- Film Database Project/connect.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as sql # Import the sqlite3 module and rename it as sql for ease of use
-
-# To use the sqlite3(sql) module: create a DB connection object, create a cursor object, execute SQL statements, commit changes, close connection
-dbCon = sql.connect(r'Film Database Project\filmflix.db') # create a new db or open an existing db.
-
-# Create a cursor object(variable) and pass/assign it to the cursor method
-dbCursor = dbCon.cursor()
